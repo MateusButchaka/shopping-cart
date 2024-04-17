9765bbf1fd1a8d7a887d8b2b4ec611273a98bfad
MateusButchaka <mateusgotardi06@gmail.com>
2024-04-17 09:58:29 -0300

    primeira commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import CartButton from '../CartButton/CartButton.jsx';
+import SearchBar from '../SearchBar/SearchBar.jsx';
+
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <SearchBar />
+        <CartButton />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useContext } from 'react';
+
+import './Products.css';
+import fetchProducts from '../../api/fetchProducts.js';
+import ProductCard from '../ProductCard/ProductCard.jsx';
+import Loading from '../Loading/Loading.jsx';
+import AppContext from '../../context/AppContext.js';
+
+function Products() {
+
+  const { products, setProducts, loading, setLoading } = useContext(AppContext);
+  
+
+  useEffect(() => {
+    fetchProducts('iphone').then((response) => {
+      setProducts(response);
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    (loading && <Loading /> ) || (
+      <section className="products container">
+        {products.map((product) => <ProductCard key={product.id} data={product} />)}
+      </section>
+    )
+    
+  );
+}
+
+export default Products;
